1784a70b011d03b1ed81517e175c0fdf0922632b
Félix Piédallu <felix@piedallu.me>
2015-08-14 11:43:58 +0200

    clean !

--- common.mk
+++ common.mk
@@ -3,12 +3,6 @@
 # Le dossier parent des différents dépôts est PARENT_DIR et doit être fourni par les
 # différents Makefile.
 
-# Options de compilations
-CFLAGS += -W -Wall -std=c99 $(TARGET) $(INCLUDE) -DDEBUG=$(DEBUG)
-
-# Options pour l'édition de liens
-LDFLAGS+= -W -Wall -std=c99
-
 # Les différents projets
 export ASSER_DIR = $(PARENT_DIR)/asservissement/
 export CARTO_DIR = $(PARENT_DIR)/cartographie/
@@ -28,6 +22,13 @@ export HARDW_LIB = $(HARDW_LIB_DIR)/$(BUILD_DIR)/lib$(ARCH).a
 
 include $(HARDW_DIR)/$(ARCH)/$(ARCH).mk
 
+# Options de compilations
+CFLAGS += -W -Wall -std=c99 $(TARGET) $(INCLUDE) -DDEBUG=$(DEBUG)
+
+# Options pour l'édition de liens
+LDFLAGS+= -L$(HARDW_LIB_DIR)/$(BUILD_DIR)/
+
+
 ################################################################################
 # Common Rules
 
@@ -41,15 +42,12 @@ $(BUILD_DIR)/%.o: %.c | $(BUILD_DIR)
 	@$(CC) $(CFLAGS) -o $@ -c $<
 
 # Compile a static library
-%.a:
+$(BUILD_DIR)/%.a:
 	@echo "	AR	$(PROJECT)|$(notdir $@)"
 	@rm -f $@
 	@$(AR) -r $@ $^
 	@echo "	RANLIB	$(PROJECT)|$(notdir $@)"
 	@$(RANLIB) $@
-
-$(BUILD_DIR):
-	@mkdir $(BUILD_DIR) $ -p
 
 ################################################################################
 # Clean the current working directory
