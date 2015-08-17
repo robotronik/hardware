903c6c9cf2aa290e00ad6cad2802ae08f95f5533
Félix Piédallu <felix@piedallu.me>
2015-08-17 15:45:40 +0200

    finalement on ne met pas ici le hardware dsPIC. + fix

--- dsPIC33F/moteurs.h
+++
@@ -1,4 +0,0 @@
-
-#define MAX_SPEED 3999  // Entrée max des PWMs = 2*Période des PWMs
-                        // Fréquence des PWMs = 20 kHz
-#define MIN_SPEED 900   // Entrée min sauf pour l'arrêt
