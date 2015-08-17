b30c89d15e99ab254c8263c46e0675c189110846
Félix Piédallu <felix@piedallu.me>
2015-08-17 14:45:06 +0200

    moved simulation to hardware/PC

--- common.mk
+++ common.mk
@@ -6,8 +6,7 @@
 # Les différents projets
 export ASSER_DIR = $(PARENT_DIR)/asservissement/
 export CARTO_DIR = $(PARENT_DIR)/cartographie/
-export COMMON_DIR= $(PARENT_DIR)/common_code/
-export COMMUNICATION_DIR= $(COMMON_DIR)/communication/
+export COMMUNICATION_DIR= $(PARENT_DIR)/common_code/communication/
 export HARDW_DIR = $(PARENT_DIR)/hardware/
 
 export STRAT_DIR = $(PARENT_DIR)/strategie/Robot_$(ROBOT)
@@ -35,7 +34,6 @@ CFLAGS += -W -Wall -std=c99 $(TARGET) $(INCLUDE) -DDEBUG=$(DEBUG)\
 # Options pour l'édition de liens
 LDFLAGS+= 	-L$(HARDW_LIB_DIR)/$(BUILD_DIR)/	\
 			-L$(CARTO_DIR)/$(BUILD_DIR)/		\
-			-L$(COMMON_DIR)/$(BUILD_DIR)/\
 			-L$(COMMUNICATION_DIR)/$(BUILD_DIR)/\
 
 
@@ -78,7 +76,6 @@ clean:
 clean-all:
 	@make clean -C $(ASSER_DIR)
 	@make clean -C $(CARTO_DIR)
-	@make clean -C $(COMMON_DIR)
 	@make clean -C $(COMMUNICATION_DIR)
 	@make clean -C $(HARDW_LIB_DIR)
 	@make clean -C $(STRAT_DIR)
@@ -86,7 +83,6 @@ clean-all:
 mrproper-all:
 	@make mrproper -C $(ASSER_DIR)
 	@make mrproper -C $(CARTO_DIR)
-	@make mrproper -C $(COMMON_DIR)
 	@make mrproper -C $(COMMUNICATION_DIR)
 	@make mrproper -C $(HARDW_LIB_DIR)
 	@make mrproper -C $(STRAT_DIR)
