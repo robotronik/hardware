37ace8081f6e480e2a03ab4242a02f2fba43368e
Félix Piédallu <salamandar@excalibur.home>
2015-11-03 20:25:26 +0100

    ajout de fonctions weak pour que la carto puisse être compilée sans simu

--- PC/hardware.c
+++ PC/hardware.c
@@ -4,3 +4,6 @@
 #include "asservissement.h"
 #include "hardware.h"
 
+void __attribute__((weak)) dessine_point_passage_carto  (void* null) { }
+void __attribute__((weak)) dessine_obstacle_rond        (void* null) { }
+void __attribute__((weak)) dessine_obstacle_ligne       (void* null) { }
